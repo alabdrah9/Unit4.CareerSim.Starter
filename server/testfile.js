059f8d0a56b5b994751f4fbfd06cd18350e9a158
alabdrah9 <aboddi1990@gmail.com>
2024-04-08 21:08:45 -0400

    remove cros from pakge. json

--- server/testfile.js
+++ server/testfile.js
@@ -13,4 +13,17 @@ const pushRandomProduct = (arr) => {
     return arr;
 }
 
-console.log(pushRandomProduct(randomProducts))
+// console.log(pushRandomProduct(randomProducts))
+
+// const randomName = faker.commerce.product();
+
+// const randomProducts = new Array(10)
+
+// const pushRandomProduct = (arr) => {
+//     for (let i = 0 ; i < arr.length ; i++) {
+//         arr[i] = faker.commerce.product()
+//     }
+//     return arr;
+// }
+
+// console.log(pushRandomProduct(randomProducts))
