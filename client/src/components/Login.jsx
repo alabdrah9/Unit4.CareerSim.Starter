b36b220aeadd1b1ce08f641def299d1f6f8610ea
alabdrah9 <aboddi1990@gmail.com>
2024-04-08 22:09:17 -0400

    fixed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,64 +1,64 @@
-// import { useState } from "react";
-// import useToken from "../UseToken.js";
+import { useState } from "react";
+import useToken from "../UseToken.js";
 
-// export default function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState();
-//   const [token, setToken] = useToken();
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+  const [token, setToken] = useToken();
 
 
-//   const handleSubmit = async (e) => {
-//    e.preventDefault()
-//     try {
-//       const response = await fetch(
-//         "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify({email, password}),
-//         }
-//       );
-//       const result = await response.json();
-//       console.log({result});
-//       if (result.token) {
-//         setToken(result.token);
-//       } else {
-//         setError(result.message);
-//       }
+  const handleSubmit = async (e) => {
+   e.preventDefault()
+    try {
+      const response = await fetch(
+        "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({email, password}),
+        }
+      );
+      const result = await response.json();
+      console.log({result});
+      if (result.token) {
+        setToken(result.token);
+      } else {
+        setError(result.message);
+      }
 
-//       console.log(result);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   return (
-//     <div>
-//       <h1>Login</h1>
-//       {error && <h2>{error}</h2>}
-//       <form onSubmit={handleSubmit}
-//       >
-//         <label htmlFor="email" />
-//         Email:{""}
-//         <input
-//           type="email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//         />
-//         <label htmlFor="password" />
-//         Password:{""}
-//         <input
-//           type="password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//                 <button type="submit">Log In</button>
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+      {error && <h2>{error}</h2>}
+      <form onSubmit={handleSubmit}
+      >
+        <label htmlFor="email" />
+        Email:{""}
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password" />
+        Password:{""}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+                <button type="submit">Log In</button>
 
-//       </form>
-//     </div>
-//   );
+      </form>
+    </div>
+  );
 
   
-// }
+}
